a1da6d62b49853b8b265e82a66c5881d938a11db
komame <h.komame.sys24@morijyobi.ac.jp>
2025-04-03 15:04:12 +0900

    利用用途を選ぶ完了#3#9

---
+++ SelectPurpose.py
@@ -0,0 +1,57 @@
+import customtkinter as ctk
+
+class SelectPurpose(ctk.CTkFrame):
+    def __init__(self, master,name,email):
+        super().__init__(master)
+        self.master = master
+        self.email = email
+        self.name = name
+
+
+        self.master.state('zoomed')
+        master.title('利用用途の確認')
+        
+        ctk.set_appearance_mode("Light")
+        ctk.set_default_color_theme("green")
+        
+
+        self.create_widgets()
+        
+        
+        
+    def create_widgets(self):
+        self.label1 = ctk.CTkLabel(self.master, text=f'{self.name} 様', font=('Times', 20)) 
+        self.label1.place(x=20, y=10)
+        self.label1 = ctk.CTkLabel(self.master, text=f'〖{self.email}〗', font=('Times', 20)) 
+        self.label1.place(x=140, y=10)
+        
+        frame = ctk.CTkFrame(self.master, width=1000, height=500)
+        frame.place(x=130, y=70)
+        
+        self.label = ctk.CTkLabel(frame, text='利用用途確認', font=('Times', 30))
+        self.label.place(x=50, y=50)
+        
+        self.stay_button = ctk.CTkButton(frame, text="宿泊", command=self.stay_event,font=('Times',40),width=300,height=300 )
+        self.stay_button.place(x=130, y= 130)
+        
+        self.banquet_button = ctk.CTkButton(frame, text="宴会", command=self.banquet_event,font=('Times',40),width=300,height=300 )
+        self.banquet_button.place(x=560, y= 130)
+        
+                
+    def stay_event(self):
+        # from auth import pagemove_select_entrystay
+        # pagemove_select_entrystay(self.name,self.email)
+        pass
+    def banquet_event(self):
+        # from auth import pagemove_select_entrybanquet
+        # pagemove_select_entrybanquet(self.name,self.email)
+        pass
+                
+        
+if __name__ == '__main__':
+    root = ctk.CTk()
+    app = SelectPurpose(root)
+    app.mainloop()
+    
+    
+ 
