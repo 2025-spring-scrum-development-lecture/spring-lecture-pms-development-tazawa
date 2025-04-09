90c2ac894c6616e82cc34f6ff3e4cd3efb8b8069
tazawaakira <a.tazawa.sys24@morijyobi.ac.jp>
2025-04-09 11:14:06 +0900

    確認画面を作る #13

--- EntryStay.py
+++ EntryStay.py
@@ -23,37 +23,40 @@ class EntryStay(ctk.CTkFrame):
         self.check_option()
         
     def create_widget(self):
+        # ゲストの名前とメールアドレスの表示
         self.guest_label = ctk.CTkLabel(self.master, text=f'{self.name}様〖{self.email}〗', font=('Times', 20))
         self.guest_label.place(x=20, y=10)
-        
+        # 確定ボタン(確認画面へ)
         self.quotation_button = ctk.CTkButton(self.master, text='確定して見積りを出す＞', font=('Times', 20), command=self.confirmed_info)
         self.quotation_button.place(x=1020, y=10)
         
+        # フレームの作成
+        base_frame = ctk.CTkFrame(self.master, width=1225, height=555)
+        base_frame.place(x=15, y=75)
+        self.canvas = ctk.CTkCanvas(base_frame, highlightthickness=0)
+        self.canvas.pack(side='left', fill='both', expand=True)
+        self.scrollbar = ctk.CTkScrollbar(base_frame, orientation='horizontal', command=self.canvas.xview)
+        self.scrollbar.pack(side='bottom', fill='x')
+        self.canvas.configure(xscrollcommand=self.scrollbar.set)
+        
         frame = ctk.CTkFrame(self.master, width=430, height=550)
         frame.place(x=20, y=80)
-        
         # 部屋数
         self.room_cnt_label = ctk.CTkLabel(frame, text=f'{self.room_cnt}部屋目', font=('Times', 16))
         self.room_cnt_label.place(x=10, y=10)
-        
-        
         # 人数(self.numAdult, self.numChild)
         self.numPeople_label = ctk.CTkLabel(frame, text='人数', font=('Times', 20))
         self.numPeople_label.place(x=10, y=40)
-        
         self.numAdult_label = ctk.CTkLabel(frame, text='大人', font=('Times', 16))
         self.numAdult_label.place(x=100, y=40)
         numAdult_menu = [1, 2, 3, 4, 5, 6]
         self.numAdult_Entry = ctk.CTkOptionMenu(frame, values=[str(num) for num in numAdult_menu], width=60, height=25, font=('Times', 16))
         self.numAdult_Entry.place(x=150, y=40)
-        
         self.numChild_label = ctk.CTkLabel(frame, text='小学生', font=('Times', 16))
         self.numChild_label.place(x=240, y=40)
         numChild_menu = [1, 2, 3, 4, 5, 6]
         self.numChild_Entry = ctk.CTkOptionMenu(frame, values=[str(num) for num in numChild_menu], width=60, height=25, font=('Times', 16))
         self.numChild_Entry.place(x=310, y=40)
-        
-        
         # 泊数(self.numStay)
         self.numStay_label = ctk.CTkLabel(frame, text='泊数', font=('Times', 20))
         self.numStay_label.place(x=10, y=80)
@@ -61,37 +64,28 @@ class EntryStay(ctk.CTkFrame):
         self.numStay_Entry.place(x=100, y=80)
         self.label = ctk.CTkLabel(frame, text='泊', font=('Times', 16))
         self.label.place(x=165, y=80)
-        
-        
         # 部屋プラン(self.room)
         self.room_label = ctk.CTkLabel(frame, text='お部屋', font=('Times', 20))
         self.room_label.place(x=10, y=120)
         room_menu = ['本館和室7.5畳', '西館洋室(ツイン)', '岩手山側和室', '岩手山側露天風呂付き和室', '西館和室10畳', '檜内風呂付き和洋室', '西館和洋室', '西館和室28畳']
         self.room_Entry = ctk.CTkOptionMenu(frame, values=[room for room in room_menu], font=('Times', 16))
         self.room_Entry.place(x=80, y=120)
-        
-        
         # 夕食プラン(self.dinner)
         self.dinner_label = ctk.CTkLabel(frame, text='ご夕食', font=('Times', 20))
         self.dinner_label.place(x=10, y=160)
         dinner_menu = ['スタンダード', '岩手県産牛', '前沢牛', '夕食なし(素泊まり)']
         self.dinner_Entry = ctk.CTkOptionMenu(frame, values=[dinner for dinner in dinner_menu], font=('Times', 16))
         self.dinner_Entry.place(x=80, y=160)
-        
-        
         # オプション(self.checkin_option, bedrockButh_option, peterAdult_option, peterChild_option, parkAdult_option, parkChild_option, tennis_option, hotSpringRental_option, dogone_option, numdog, dogoneSpa)
         self.option_label = ctk.CTkLabel(frame, text='オプション', font=('Times', 20))
         self.option_label.place(x=10, y=200)
-
         self.option_menu = ['土曜日チェックイン', '岩盤浴', 'パターゴルフ', 'パークゴルフ', 'テニスコート利用', '温泉貸し切り', 'ドッグワン']
         self.option_vars = []
-
         for i, option in enumerate(self.option_menu):
             var = ctk.StringVar(value="off")
             self.option_vars.append(var)
             checkbox = ctk.CTkCheckBox(frame, text=option, variable=var, onvalue="on", offvalue="off", font=('Times', 16), checkbox_width=15, checkbox_height=15)
             checkbox.place(x=10, y=240 + i * 30)
-            
         self.numDog_var = ctk.IntVar()
         self.numDog_var.set(1)
         self.numDog_Entry = ctk.CTkEntry(frame, width=60, height=25, font=('Times', 16), textvariable=self.numDog_var)
@@ -101,7 +95,8 @@ class EntryStay(ctk.CTkFrame):
         self.dogoneSpaUse_label = ctk.CTkRadioButton(frame, text='利用する', font=('Times', 16), variable=self.radio_vars, value='use')
         self.dogoneSpaUnUse_label = ctk.CTkRadioButton(frame, text='利用しない', font=('Times', 16), variable=self.radio_vars, value='unuse')
         
-    def check_option(self):
+    def check_option(self):  # オプション選択の有無のチェック、金額の計算
+        # チェックボックスのフラグ
         checkin_selected = False
         bedrockButh_selected = False
         petergolf_selected = False
@@ -109,25 +104,25 @@ class EntryStay(ctk.CTkFrame):
         tennis_selected = False
         hotSpringRental_selected = False
         dogone_selected = False
-        
+        # 人数の取得
         self.numAdult = int(self.numAdult_Entry.get())
         self.numChild = int(self.numChild_Entry.get())
-        
+        # 入力中に宿泊数を空文字にしないためのtry-except
         try:
             self.numStay = self.numStay_Entry.get()
             if self.numStay == '':
                 self.numStay = 1
         except Exception as e:
             self.numDog = 1
-        
+        # プランの取得
         self.room = self.room_Entry.get()
         self.dinner = self.dinner_Entry.get()
         self.plan = self.calc_price(self.room, self.dinner)
         self.planPrace_Adult = self.plan[0]
         self.planPrace_Child = self.plan[1]
-        
+        # 大人と小学生の合計人数を計算する
         self.numPeople = self.numAdult + self.numChild
-        
+        # チェックボックスの判定
         for i, var in enumerate(self.option_vars):
             if var.get() == 'on':
                 if self.option_menu[i] == '土曜日チェックイン':
@@ -144,53 +139,55 @@ class EntryStay(ctk.CTkFrame):
                     hotSpringRental_selected = True
                 elif self.option_menu[i] == 'ドッグワン':
                     dogone_selected = True
-                    
+        # 土曜日チェックイン料金
         if checkin_selected:
             self.checkin_option = 2200 * self.numPeople
         else:
             self.checkin_option = 0
-              
+        # 岩盤浴料金
         if bedrockButh_selected:
             self.bedrockButh_option = 1500 * self.numPeople
         else:
             self.bedrockButh_option = 0
-            
+        # パターゴルフ料金
         if petergolf_selected:
             self.peterAdult_option = 500 * self.numAdult
             self.peterChild_option = 250 * self.numChild
         else:
             self.peterAdult_option = 0
             self.peterChild_option = 0
-            
+        # パークゴルフ料金
         if pardgolf_selected:
             self.parkAdult_option = 600 * self.numAdult
             self.parkChild_option = 300 * self.numChild
         else:
             self.parkAdult_option = 0
             self.parkChild_option = 0
-            
+        # テニスコート利用料金
         if tennis_selected:
             self.tennis_option = 1000 * self.numPeople
         else:
             self.tennis_option = 0
-            
+        # 温泉貸し切り料金
         if hotSpringRental_selected:
             self.hotSpringRental_option = 2500 * self.numPeople
         else:
             self.hotSpringRental_option = 0
-                    
+        # ドッグワン料金
         if dogone_selected:
             self.numDog_Entry.place(x=130, y=420)
             self.numDog_label.place(x=195, y=420)
             self.dogoneSpa_label.place(x=130, y=460)
             self.dogoneSpaUse_label.place(x=170, y=460)
             self.dogoneSpaUnUse_label.place(x=170, y=490)
+            # 犬の数の入力中に空文字にしないためのtry-except
             try:
                 self.numDog = self.numDog_var.get()
                 if self.numDog == '':
                     self.numDog = 0
             except Exception as e:
                 self.numDog = 1
+            # 料金の計算
             self.numDog_option = 1000
             if self.radio_vars.get() == 'use':
                 self.dogoneSpa_option = 1800
@@ -202,7 +199,6 @@ class EntryStay(ctk.CTkFrame):
             numDog = int(self.numDog)
             dogoneSpa_option = int(self.dogoneSpa_option)
             self.dogone_option = numDog_option * numDog + dogoneSpa_option
-            
         else:
             self.numDog_Entry.place_forget()
             self.numDog_label.place_forget()
@@ -210,10 +206,10 @@ class EntryStay(ctk.CTkFrame):
             self.dogoneSpaUse_label.place_forget()
             self.dogoneSpaUnUse_label.place_forget()
             self.dogone_option = 0
-                    
+        # 毎秒チェックする
         self.master.after(1000, self.check_option)
         
-    def calc_price(self, room, dinner):
+    def calc_price(self, room, dinner):  # 部屋と夕食から料金を出す
         self.plan_menu = {('本館和室7.5畳', 'スタンダード'): (12400, 12400),
                 ('本館和室7.5畳', '岩手県産牛'): (15400, 15400),
                 ('本館和室7.5畳', '前沢牛'): (18400, 18400),
@@ -256,7 +252,7 @@ class EntryStay(ctk.CTkFrame):
         self.plan = self.plan_menu.get((room, dinner))
         return self.plan
         
-    def confirmed_info(self):
+    def confirmed_info(self):  # 確定ボタンイベント
         # self.numAdult = self.numAdult_Entry.get()
         # self.numChild = self.numChild_Entry.get()
         # self.numStay = self.numStay_Entry.get()
@@ -278,7 +274,7 @@ class EntryStay(ctk.CTkFrame):
         
         from auth import pagemove_entrystay_quotationstay
         self.destroy()
-        pagemove_entrystay_quotationstay(self.master, self.numAdult, self.numChild, self.numStay, self.planPrace_Adult, self.planPrace_Child, self.checkin_option, self.bedrockButh_option, self.peterAdult_option, self.peterChild_option, self.parkAdult_option, self.parkChild_option, self.tennis_option, self.hotSpringRental_option, self.dogone_option)
+        pagemove_entrystay_quotationstay(self.master, self.name, self.email, self.numAdult, self.numChild, self.numStay, self.room, self.dinner, self.planPrace_Adult, self.planPrace_Child, self.checkin_option, self.bedrockButh_option, self.peterAdult_option, self.peterChild_option, self.parkAdult_option, self.parkChild_option, self.tennis_option, self.hotSpringRental_option, self.dogone_option)
 
 if __name__ == '__main__':
     root = ctk.CTk()
---
+++ QuotationStay.py
@@ -0,0 +1,155 @@
+import customtkinter as ctk
+from tkinter import messagebox
+
+class QuotationStay(ctk.CTkFrame):
+    def __init__(self, master, name, email, numAdult, numChild, numStay, room, dinner, planPrace_Adult, planPrace_Child, checkin_option, bedrockButh_option, peterAdult_option, peterChild_option, parkAdult_option, parkChild_option, tennis_option, hotSpringRental_option, dogone_option, total):
+        
+        super().__init__(master)
+        
+        self.master = master
+        self.name = name
+        self.email = email
+        self.numAdult = numAdult
+        self.numChild = numChild
+        self.numPeople = numAdult + numChild
+        self.numStay = numStay
+        self.room = room
+        self.dinner = dinner
+        self.planPrace_Adult = planPrace_Adult
+        self.planPrace_Child = planPrace_Child
+        self.checkin_option = checkin_option
+        self.bedrockButh_option = bedrockButh_option
+        self.peterAdult_option = peterAdult_option
+        self.peterChild_option = peterChild_option
+        self.parkAdult_option = parkAdult_option
+        self.parkChild_option = parkChild_option
+        self.tennis_option = tennis_option
+        self.hotSpringRental_option = hotSpringRental_option
+        self.dogone_option = dogone_option
+        self.total = total
+        
+        screen_width = master.winfo_screenwidth()
+        screen_height = master.winfo_screenheight()
+        master.geometry(f'{screen_width}x{screen_height}')
+        master.title('入力内容の確認')
+
+        ctk.set_appearance_mode("Light")
+        ctk.set_default_color_theme("green")
+        
+        self.create_widget()
+        
+    def create_widget(self):
+        # ゲストの名前とメールアドレスの表示
+        self.guest_label = ctk.CTkLabel(self.master, text=f'{self.name}様〖{self.email}〗', font=('Times', 20))
+        self.guest_label.place(x=20, y=10)
+        # 入力内容の変更ボタン
+        self.change_button = ctk.CTkButton(self.master, text='入力に戻る', font=('Times', 16), width=170, height=40, command=self.change_button_event)
+        self.change_button.place(x=900, y=10)
+        # データ保存ボタン
+        self.confirm_button = ctk.CTkButton(self.master, text='確定してTopへ', font=('Times', 16), width=170, height=40, command=self.confirm_button_event)
+        self.confirm_button.place(x=1080, y=10)
+        
+        # ベースフレームの作成(表示サイズの固定)
+        base_frame = ctk.CTkFrame(self.master, width=800, height=550)
+        base_frame.place(x=250, y=60)
+        base_frame.pack_propagate(False)
+        # スクロールフレームの作成(スクロール機能の追加)
+        scrollable_frame = ctk.CTkScrollableFrame(base_frame, width=800, height=550)
+        scrollable_frame.pack(fill="both", expand=True)
+        # 内部フレームの作成(レイアウト用)
+        frame = ctk.CTkFrame(scrollable_frame, fg_color="transparent", height=700)
+        frame.pack(fill="both", expand=True)
+        frame.pack_propagate(False)
+
+        # 見積り結果の表示
+        self.result_label = ctk.CTkLabel(frame, text='お見積り結果', font=('Times', 20))
+        self.result_label.place(x=60, y=20)
+        self.result = ctk.CTkLabel(frame, text=f'{self.total}円', font=('Times', 20))
+        self.result.place(x=210, y=20)
+        
+        # 詳細の表示
+        self.breakdown_label = ctk.CTkLabel(frame, text='詳細', font=('Times', 20))
+        self.breakdown_label.place(x=60, y=80)
+        # 人数（大人／小学生）
+        self.numPeople_label = ctk.CTkLabel(frame, text=f'人数（大人／小学生）　{self.numPeople}名様（{self.numAdult}名／{self.numChild}名）', font=('Times', 16))
+        self.numPeople_label.place(x=100, y=120)    
+        # 泊数
+        self.numStay_label = ctk.CTkLabel(frame, text=f'泊数　{self.numStay}泊', font=('Times', 16))
+        self.numStay_label.place(x=100, y=160)
+        # プラン
+        self.planroom_label = ctk.CTkLabel(frame, text=f'プラン　お部屋［ {self.room} ］', font=('Times', 16))
+        self.planroom_label.place(x=100, y=200)
+        self.plandinner_label = ctk.CTkLabel(frame, text=f'ご夕食［ {self.dinner} ］', font=('Times', 16))
+        self.plandinner_label.place(x=162, y=230)
+        self.planPrace_Adult_prace = ctk.CTkLabel(frame, text=f'大人 {self.planPrace_Adult * self.numAdult}円', font=('Times', 16))
+        self.planPrace_Adult_prace.place(relx=1.0, x=-100, y=230, anchor='e')
+        self.planPrace_Child_prace = ctk.CTkLabel(frame, text=f'小学生 {self.planPrace_Child * self.numChild}円', font=('Times', 16))
+        self.planPrace_Child_prace.place(relx=1.0, x=-100, y=260, anchor='e')
+        # 土曜日チェックイン
+        self.checkin_option_label = ctk.CTkLabel(frame, text='土曜日チェックイン', font=('Times', 16))
+        self.checkin_option_label.place(x=100, y=285)
+        self.checkin_option_prace = ctk.CTkLabel(frame, text=f'{self.checkin_option}円', font=('Times', 16))
+        self.checkin_option_prace.place(relx=1.0, x=-100, y=300, anchor='e')
+        # 岩盤浴
+        self.bedrockButh_option_label = ctk.CTkLabel(frame, text='岩盤浴', font=('Times', 16))
+        self.bedrockButh_option_label.place(x=100, y=325)
+        self.bedrockButh_option_prace = ctk.CTkLabel(frame, text=f'{self.bedrockButh_option}円', font=('Times', 16))
+        self.bedrockButh_option_prace.place(relx=1.0, x=-100, y=340, anchor='e')
+        # パターゴルフ
+        self.petergolf_option_label = ctk.CTkLabel(frame, text='パターゴルフ', font=('Times', 16))
+        self.petergolf_option_label.place(x=100, y=365)
+        self.peterAdult_option_price = ctk.CTkLabel(frame, text=f'大人{self.peterAdult_option}円', font=('Times', 16))
+        self.peterAdult_option_price.place(relx=1.0, x=-100, y=380, anchor='e')
+        self.peterChild_option_price = ctk.CTkLabel(frame, text=f'小学生{self.peterChild_option}円', font=('Times', 16))
+        self.peterChild_option_price.place(relx=1.0, x=-100, y=410, anchor='e')
+        # パークゴルフ
+        self.parkgolf_option_label = ctk.CTkLabel(frame, text='パークゴルフ', font=('Times', 16))
+        self.parkgolf_option_label.place(x=100, y=435)
+        self.parkAdult_option_prace = ctk.CTkLabel(frame, text=f'大人{self.parkAdult_option}円', font=('Times', 16))
+        self.parkAdult_option_prace.place(relx=1.0, x=-100, y=450, anchor='e')
+        self.parkChild_option_prace = ctk.CTkLabel(frame, text=f'小学生{self.parkChild_option}円', font=('Times', 16))
+        self.parkChild_option_prace.place(relx=1.0, x=-100, y=480, anchor='e')
+        # テニスコート利用
+        self.tennis_option_label = ctk.CTkLabel(frame, text='テニスコート', font=('Times', 16))
+        self.tennis_option_label.place(x=100, y=505)
+        self.tennis_option_prace = ctk.CTkLabel(frame, text=f'{self.tennis_option}円', font=('Times', 16))
+        self.tennis_option_prace.place(relx=1.0, x=-100, y=520, anchor='e')
+        # 温泉貸し切り
+        self.hotSpringRental_option_label = ctk.CTkLabel(frame, text='温泉貸し切り', font=('Times', 16))
+        self.hotSpringRental_option_label.place(x=100, y=545)
+        self.hotSpringRental_option_prace = ctk.CTkLabel(frame, text=f'{self.hotSpringRental_option}円', font=('Times', 16))
+        self.hotSpringRental_option_prace.place(relx=1.0, x=-100, y=560, anchor='e')
+        # ドッグワン
+        self.dogone_option_label = ctk.CTkLabel(frame, text='ドッグワン', font=('Times', 16))
+        self.dogone_option_label.place(x=100, y=585)
+        self.dogone_option_prace = ctk.CTkLabel(frame, text=f'{self.dogone_option}円', font=('Times', 16))
+        self.dogone_option_prace.place(relx=1.0, x=-100, y=600, anchor='e')
+        # 横線を引く
+        line = ctk.CTkFrame(frame, height=2, width=620, fg_color="gray")
+        line.place(x=80, y=620)
+        # 合計
+        self.total_label = ctk.CTkLabel(frame, text='合計', font=('Times', 16))
+        self.total_label.place(x=100, y=630)
+        self.total_prace = ctk.CTkLabel(frame, text=f'{self.total}円', font=('Times', 16))
+        self.total_prace.place(relx=1.0, x=-100, y=642, anchor='e')
+        
+    def change_button_event(self):  # 入力画面に戻るイベント
+        self.destroy()
+        # from auth import
+    
+    def confirm_button_event(self):  # 確定ボタンのイベント(データ保存、メール送信、Topページへ)
+        self.storage = messagebox.askyesnocancel('データ保存', '見積り内容をjsonファイルに保存しますか？')
+        if self.storage:
+            pass
+        else:
+            pass
+        self.send_mail = messagebox.askyesno('メール送信', f'見積り結果を送信しますか？\n{self.email}に送信')
+        if self.send_mail:
+            pass
+        else:
+            pass
+    
+if __name__ == '__main__':
+    root = ctk.CTk()
+    app = QuotationStay(root, name='aaa', email='aaa', numAdult=2, numChild=2, numStay=1, room='西館和洋室',dinner='スタンダード', planPrace_Adult=0, planPrace_Child=0, checkin_option=0, bedrockButh_option=0, peterAdult_option=0, peterChild_option=0, parkAdult_option=0, parkChild_option=0, tennis_option=0, hotSpringRental_option=0, dogone_option=0, total=0)
+    root.mainloop()
